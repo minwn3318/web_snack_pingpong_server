76913028e2ff5b013aed875bd92bfa050bd0025d
minwn3318 <minwn3318@naver.com>
2025-05-11 16:56:36 +0900

    make api head

--- ShootingMiner/src/main/java/shooting_miner/example/web_snack_pingpong_server/controller/FrontController.java
+++ ShootingMiner/src/main/java/shooting_miner/example/web_snack_pingpong_server/controller/FrontController.java
@@ -1,23 +1,39 @@
 package shooting_miner.example.web_snack_pingpong_server.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ReguestMapping("/shooting-miner")
+@RequestMapping("/shooting-miner")
 public class FrontController {
 
     @Autowired
     private JPAinterface jpainterface;
 
-    @GetMapping(value = "/max-rank-tire") 
-    public void ReadMaxRankTire(@RequestParam String userID) {}
+    @PostMapping("/ranking")
+    public void addRanking(@RequestBody String json) { }
+
+    @GetMapping("/ranking/top-users") 
+    public void getTopUsers(@RequestParam("total") List<Integer> totals) { }
     
+    @GetMapping("/ranking/{userId}/max-stage")
+    public void getUserMaxStage(@PathVariable String userId) { }
+    
+    @GetMapping("/ranking/{userId}/max-score")
+    public void getUserMaxScore(@PathVariable String userId) { }
+
+    @GetMapping("/ranking/{userId}/max-total")
+    public void getUserMaxTotal(@PathVariable String userId) { }
+
     @PostMapping(value = "/update-rank-tire")
     public String UpdateRankTire(@RequestBody String json) {
         try {
